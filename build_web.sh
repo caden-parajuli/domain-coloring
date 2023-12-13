b64c24f011bf848102148a1da5c45d1d9f9a5c57
Caden Parajuli <cadenparajuli@vivaldi.net>
2023-12-12 23:16:47 -0500

    Fixed web base href

--- build_web.sh
+++ build_web.sh
@@ -20,5 +20,5 @@ export CARGO_TERM_COLOR='always'
 
 wasm-pack build -t no-modules -d $wasmOutput --no-typescript --out-name $crateName --release $crateDir -- -Z build-std=std,panic_abort
 wasm-bindgen $crateDir/target/wasm32-unknown-unknown/release/$crateName.wasm --out-dir $wasmOutput --no-typescript --target no-modules
-flutter build web
+flutter build web --base-href "/domain-coloring/"
 chmod -c -R +rX "build/web/"
